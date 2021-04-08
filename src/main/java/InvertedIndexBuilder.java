cdfc85a24d5a30d18a1e020751d9f59bedb1ceb0
clsy <clsy@dons.usfca.edu>
2021-04-08 10:45:40 +0800

    Added Builder Class for Query Result

--- src/main/java/InvertedIndexBuilder.java
+++ src/main/java/InvertedIndexBuilder.java
@@ -3,10 +3,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collection;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 import opennlp.tools.stemmer.Stemmer;
 import opennlp.tools.stemmer.snowball.SnowballStemmer;
@@ -75,50 +71,5 @@ public class InvertedIndexBuilder {
 				}
 			}
 		}
-	}
-	
-	/**
-	 * Stores single search results
-	 */
-	private final Map<String, Collection<SingleSearchResult>> results = new TreeMap<>();
-	
-	/**
-	 * Cleans, parses and sorts each query line
-	 * 
-	 * @param path the path of the file
-	 * @param exact to check if exact/partial search
-	 * @throws IOException if an IO error occurs
-	 */
-	public void parseQuery(Path path, boolean exact) throws IOException {
-		results.clear();
-		try (BufferedReader read = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
-			String line;
-			while ((line = read.readLine()) != null) {
-				//assigns set a set of unique, cleaned and stemmed words parsed from line
-				TreeSet<String> set = TextFileStemmer.uniqueStems(line);
-				if (exact == true) {
-					if (!set.isEmpty()) {
-						//puts a new string from set separated by spaces as key and list of search results as value
-						results.put(String.join(" ", set), invertedIndex.exactSearch(set));
-					}
-				}
-				else {
-					if (!set.isEmpty()) {
-						results.put(String.join(" ", set), invertedIndex.partialSearch(set));
-					}
-				}
-			}
-		}
-	}
-	
-	
-	/**
-	 * Calls SimpleJsonWriter's asNestedResult method
-	 * 
-	 * @param path the path given by user or default path if otherwise
-	 * @throws IOException if an IO error occurs
-	 */
-	public void toJsonNestedResult(Path path) throws IOException {
-		SimpleJsonWriter.asNestedResult(results, path);
 	}
 }
