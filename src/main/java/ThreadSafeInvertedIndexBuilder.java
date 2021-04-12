2fca16930f66b345cf699b195758f75d381d15cc
clsy <clsy@dons.usfca.edu>
2021-04-12 10:05:25 +0800

    Working on MultiThreading

--- src/main/java/ThreadSafeInvertedIndexBuilder.java
+++ src/main/java/ThreadSafeInvertedIndexBuilder.java
@@ -1,5 +1,13 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Set;
+
+import opennlp.tools.stemmer.Stemmer;
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
+import opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM;
 
 //make thread safe class that "public class Task" inner class that goes through each file
 //create local index
@@ -8,20 +16,43 @@ import java.nio.file.Path;
 
 public class ThreadSafeInvertedIndexBuilder extends InvertedIndexBuilder {
 	
+	private final ConcurrentInvertedIndex index;
 	private final WorkQueue queue;
 	
-	public ThreadSafeInvertedIndexBuilder(WorkQueue queue, InvertedIndex invertedIndex) {
-		super(invertedIndex);
+	public ThreadSafeInvertedIndexBuilder(ConcurrentInvertedIndex index, WorkQueue queue) {
+		super(index);
+		this.index = index;
 		this.queue = queue;
 	}
 	
 	@Override
 	public synchronized void add(Path path) throws IOException {
 		super.add(path);
+//		queue.finish();
 	}
 	
 	@Override 
 	public synchronized void addData(Path path) throws IOException {
-		super.addData(path);
+//		super.addData(path);
+		Task task = new Task(path);
+		queue.execute(task);
+	}
+	
+//	WorkQueue task = new WorkQueue();
+	
+	private class Task implements Runnable {
+		private final Path path;
+		
+		public Task(Path path) {
+			this.path = path;
+//			task.incrementPending(); 
+		}
+		@Override
+		public void run() {
+			InvertedIndex local = new InvertedIndex();
+			InvertedIndexBuilder.addData(path, local);
+			
+		}
+		
 	}
 }
