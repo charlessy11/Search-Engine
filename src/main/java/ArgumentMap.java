0a3f7c50e8e4a45f9798e08c556a8e7513e3b60a
clsy <clsy@dons.usfca.edu>
2021-04-16 20:26:35 +0800

    Merge P3 Branch

--- src/main/java/ArgumentMap.java
+++ src/main/java/ArgumentMap.java
@@ -183,6 +183,17 @@ public class ArgumentMap {
 	}
 
 	/**
+	 * Returns the value the specified flag is mapped as an int value.
+	 *
+	 * @param flag the flag whose associated value will be returned
+	 * @return the value the specified flag is mapped as a int, or the default
+	 *         value if there is no valid mapping
+	 */
+	public int getInteger(String flag) {
+			return Integer.parseInt(map.get(flag));
+	}
+	
+	/**
 	 * Returns the value the specified flag is mapped as an int value, or the
 	 * default value if unable to retrieve this mapping (including being unable to
 	 * convert the value to an int or if no value exists).
@@ -198,23 +209,6 @@ public class ArgumentMap {
 			return Integer.parseInt(map.get(flag));
 		}
 		return defaultValue;
-	}
-	
-	/**
-	 * Returns the value the specified flag is mapped as an int value, or the
-	 * default value if unable to retrieve this mapping (including being unable to
-	 * convert the value to an int or if no value exists).
-	 *
-	 * @param flag the flag whose associated value will be returned
-	 * @param defaultValue the default value to return if there is no valid
-	 *        mapping
-	 * @return the value the specified flag is mapped as a int, or the default
-	 *         value if there is no valid mapping
-	 */
-	public int getInteger(String flag) {
-		if (hasValue(flag)) {
-			return Integer.parseInt(map.get(flag));
-		}
 	}
 
 	@Override
